16996066628ddd1e259794951dfbf98b5b70448f
quixote15 <santos2tiago@gmail.com>
2017-07-08 11:09:23 -0300

    Corrigir erros na tela nivel

--- src/app/nivel/nivel.component.ts
+++ src/app/nivel/nivel.component.ts
@@ -6,16 +6,36 @@ import { Component, OnInit } from '@angular/core';
  
 })
 export class NivelComponent implements OnInit {
-
+    nivelBaril = 400.0;
     totalCopos:number = 1;
-    valorCopo = 500; //um copo === 500 ml
+    capacidade = 30; //15 litros
+    litros = 30; 
+    vazao = 0;
+    tempo = 5;
+    valorCopo = 0.5; //um copo === 500 ml
 
     constructor() { }
 
     ngOnInit() { }
 
 
+    /**
+     *  Regra de 3
+     *  se 400 cm - 30L
+     *      Xcm - 0,5L ??
+     *  resposta = 13,33 cm
+     * 
+     */
+
     nivelCalcutations(){
+        let subCm = 6.7;
+        console.log(this.totalCopos);
+        if(this.totalCopos*subCm < this.nivelBaril){
+            this.nivelBaril = this.nivelBaril - (this.totalCopos*subCm);
+            this.litros = this.litros - (this.valorCopo*this.totalCopos);
+            this.vazao = (this.valorCopo*this.totalCopos)/ this.totalCopos * 5; //Total de 
+        }
+            
 
     }
 }
